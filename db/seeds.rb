c2ea252e7c5220b5d9c3c47240291c6dbbdb6b7f
amy hastings <amyh364@outlook.com>
2024-04-24 18:49:36 +0100

    challenge 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Villa spain',
+  address: 'barcelona',
+  description: 'A lovely summer feel for this villa. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 125,
+  number_of_guests: 6
+)
+Flat.create!(
+  name: 'glamping pod',
+  address: 'dorset',
+  description: 'glamping pod with hot tub',
+  price_per_night: 60,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'chalet',
+  address: 'St Anton',
+  description: 'ski in ski out challet',
+  price_per_night: 600,
+  number_of_guests: 6
+)
